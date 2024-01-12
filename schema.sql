75359f7efe46206a7fc36c423ea4c36135d779cc
APPLE <wenmingyan1990@163.com>
2024-01-12 17:29:35 +0800

    数据添加

--- schema.sql
+++ schema.sql
@@ -12,6 +12,19 @@ CREATE TABLE "tb_user" (
     "icon" text,
     "register_time" INTEGER default 0
 );
+drop table if exists tb_message;
+CREATE TABLE "tb_message" (
+    "msgId" INTEGER primary key autoincrement,
+    "from_entity" INTEGER,
+    "from_id" INTEGER,
+    "to_id" INTEGER,
+    "to_entity" INTEGER,
+    "message_type" INTEGER,
+    "content" TEXT,
+    "status" integer,
+    "timestamp" INTEGER NOT null default 0
+);
+
 drop table if exists tb_security_code;
 CREATE TABLE "tb_security_code" (
     "codeId" INTEGER PRIMARY KEY autoincrement,
@@ -27,22 +40,21 @@ CREATE TABLE "tb_token" (
     "token" TEXT,
     "is_expire" INTEGER NOT null default 0
 );
-drop table if exists tb_message;
-CREATE TABLE "tb_message" (
-    "msgId" INTEGER primary key autoincrement,
-    "from_entity" INTEGER,
-    "from_id" INTEGER,
-    "to_id" INTEGER,
-    "to_entity" INTEGER,
-    "message_type" INTEGER,
-    "content" TEXT,
-    "status" integer,
-    "timestamp" INTEGER NOT null default 0
-);
 
 drop table if exists tb_user_address;
 create table "tb_user_address" (
     'id' integer primary key autoincrement ,
     "userId" integer,
     "addressUserId" integer
-)
+);
+
+drop table if exists tb_file;
+CREATE TABLE "tb_file" (
+    "fileId" INTEGER primary key autoincrement,
+    "filePath" Text,
+    "file_md5" INTEGER
+);
+
+
+
+SELECT msgId,from_id,from_entity,from_entity,to_id,to_entity,content,message_type,timestamp,status FROM tb_message WHERE (status = 0)
